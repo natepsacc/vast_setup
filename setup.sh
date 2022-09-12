802018cf36f683215a95b3bf126ab06fd005d605
ihateyou <natepsacc@gmail.com>
2022-09-12 15:32:39 -0400

    1

---
+++ setup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/google/mipnerf.git; cd mipnerf && conda create --name mipnerf python=3.6.13; conda activate mipnerf && conda install pip; pip install --upgrade 
+pip && pip install -r requirements.txt && pip install --upgrade jax jaxlib==0.1.65+cuda101 -f https://storage.googleapis.com/jax-releases/jax_releases.html && sudo 
+add-apt-repository ppa:alessandro-strada/ppa && sudo apt update && sudo apt install google-drive-ocamlfuse && google-drive-ocamlfuse
